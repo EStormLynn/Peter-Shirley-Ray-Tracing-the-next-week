474a929e3bf53ebbaac22ee8e8d14edb67eeb26b
EStormLynn <zhang3158627@sina.com>
2018-10-16 16:37:31 +0800

    test Cornell box
    main.cpp

--- Peter-Shirley-Project Code/main.cpp
+++ Peter-Shirley-Project Code/main.cpp
@@ -12,6 +12,7 @@
 
 #include "stb_image.h"
 #include "stb_image_write.h"
+#include "aarect.h"
 
 
 #define random(a, b) (rand()%(b-a+1)+a)
@@ -25,17 +26,16 @@ vec3 color(const ray &r, hitable *world, int depth) {
         ray scattered;
         // 衰减
         vec3 attenuation;
-
+        // 记录自发光的颜色
+        vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
         if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
             // 递归 衰减
-            return attenuation * color(scattered, world, depth + 1);
+            return emitted + attenuation * color(scattered, world, depth + 1);
         } else {
-            return vec3(0, 0, 0);
+            return emitted;
         }
     } else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+        return vec3(0, 0, 0);
     }
 }
 
@@ -81,40 +81,100 @@ hitable *random_scene() {
 hitable *earth() {
     int nx, ny, nn;
     unsigned char *tex_data = stbi_load("picture.png", &nx, &ny, &nn, 0);
-    material *mat =  new lambertian(new image_texture(tex_data, nx, ny));
-    return new sphere(vec3(0,2, 0), 2, mat);
+    material *mat = new lambertian(new image_texture(tex_data, nx, ny));
+    return new sphere(vec3(0, 2, 0), 2, mat);
 }
 
-hitable *two_perlin_spheres()
-{
+hitable *two_perlin_spheres() {
     texture *pertext = new noise_texture();
-    hitable **list = new hitable*[2];
-    list[0] = new sphere(vec3(0,-1000,0),1000,new lambertian(pertext));
+    hitable **list = new hitable *[2];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
 //    list[1] = new sphere(vec3(0,2,0),2,new lambertian(pertext));
     list[1] = earth();
-    return new hitable_list(list,2);
+    return new hitable_list(list, 2);
 }
+
+// 带rect和光源的场景
+hitable *simple_light() {
+    texture *pertext = new noise_texture(4);
+    texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)),
+                                           new constant_texture(vec3(0.9, 0.9, 0.9)));
+    hitable **list = new hitable *[4];
+    list[0] = new sphere(vec3(0, -700, 0), 700, new lambertian(checker));
+    list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+    list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    list[3] = new xy_rect(3, 5, 1, 3, -2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    return new hitable_list(list, 4);
+}
+
+hitable *test() {
+    texture *pertext = new noise_texture(4);
+    texture *checker = new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)),
+                                           new constant_texture(vec3(0.9, 0.9, 0.9)));
+    hitable **list = new hitable *[4];
+    list[0] = new sphere(vec3(0, -700, 0), 700, new lambertian(checker));
+    list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+    list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(11, 11, 11))));
+//    list[3] = new xy_rect(3,5,1,3,-2,new diffuse_light(new constant_texture(vec3(4,4,4))));
+    return new hitable_list(list, 3);
+}
+
+// cornell_box经典场景
+hitable *cornell_box() {
+    hitable **list = new hitable *[8];
+    int i = 0;
+    texture *pertext = new noise_texture(4);
+    material *red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)));
+    material *white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
+    material *green = new lambertian(new constant_texture(vec3(0.12, 0.45, 0.15)));
+    material *light = new diffuse_light(new constant_texture(vec3(15, 15, 15)));
+    material *redM = new lambertian(pertext);
+//    list[i++] = new flip_normals(new yz_rect(0, 555, 0, 555, 555, green));
+    list[i++] = new yz_rect(0, 555, 0, 555, 30, light);
+    list[i++] = new yz_rect(0, 555, 0, 555, 0, redM);
+//    list[i++] = new xz_rect(0, 556, 0, 556, 0, light);
+//    list[i++] = new
+//    list[i++] = new flip_normals(new xz_rect(200, 300, 200, 300, 100, red));
+//    list[i++] = new flip_normals(new xy_rect(-200, 1555, -200, 1555, 0, light));
+    return new hitable_list(list, i);
+}
+
 
 int main() {
     string str = "";
 
-    int nx = 200;
+    int nx = 100;
     int ny = 100;
     // 采样数量ns
     int ns = 100;
 
 
-    vec3 lookfrom(13, 2, 3);
-    vec3 lookat(0, 0, 0);
+    vec3 lookfrom(278, 278, -800);
+    vec3 lookat(278, 278, 0);
     float dist_to_focus = 10.0;
     float aperture = 0.1;
-    camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
+    float vfov = 40.0;
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), vfov, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
+
+//    vec3 lookfrom(13, 2, 3);
+//    vec3 lookat(0, 0, 0);
+//    float dist_to_focus = 10.0;
+//    float aperture = 0.1;
+//    camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
 
 
-
-    hitable *world ;
 //    world = random_scene();
-    world = two_perlin_spheres();
+//    hitable *world = random_scene();
+//    hitable *world = two_spheres();
+//    hitable *world = two_perlin_spheres();
+//    hitable *world = earth();
+//    hitable *world = simple_light();
+//    hitable *world = test();
+    hitable *world = cornell_box();
+//    hitable *world = cornell_balls();
+//    hitable *world = cornell_smoke();
+//    hitable *world = cornell_final();
+//    hitable *world = final();
 
     random_device rd;
 
